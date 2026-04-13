e1be1f8b64cf61312f930ee42858b2c3596c297d
Samuel <samuelti0410@gmail.com>
2026-04-13 23:32:31 +0800

    Adding table and Create part for CRUD

--- src/database.py
+++ src/database.py
@@ -1,1 +1,22 @@
 import sqlite3
+conn = sqlite3.connect ("tracker.db")
+cursor = conn.cursor()
+
+cursor.execute ("""
+                CREATE TABLE IF NOT EXISTS tracker(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                date TEXT,
+                topic TEXT,
+                duration INTEGER,
+                weakness_level INTEGER
+                )
+""")
+
+cursor.execute("""
+INSERT INTO tracker (date, topic, duration, weakness_level)
+VALUES (?, ?, ?, ?)
+""",("2026-04-13", "Math", 60, 5)
+)
+
+conn.commit()
+conn.close()
