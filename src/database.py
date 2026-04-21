279e6728d02c17fd77889cdb7109812b443f1b6b
Samuel <samuelti0410@gmail.com>
2026-04-21 15:34:37 +0800

    Added tracker how many times a subject is study and improvement on INSERT

--- src/database.py
+++ src/database.py
@@ -1,22 +1,26 @@
 import sqlite3
 conn = sqlite3.connect ("tracker.db")
-cursor = conn.cursor()
+cursor = conn.cursor ()
 
 cursor.execute ("""
                 CREATE TABLE IF NOT EXISTS tracker(
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
-                date TEXT,
                 topic TEXT,
                 duration INTEGER,
                 weakness_level INTEGER
                 )
 """)
 
-cursor.execute("""
-INSERT INTO tracker (date, topic, duration, weakness_level)
-VALUES (?, ?, ?, ?)
-""",("2026-04-13", "Math", 60, 5)
+cursor.execute ("""
+INSERT INTO tracker (topic, duration, weakness_level)
+VALUES (?, ?, ?)
+""",(X, Y, Z)
 )
 
-conn.commit()
-conn.close()
+cursor.execute ("""
+SELECT * FROM tracker
+                """)
+print (cursor.fetchall())
+
+conn.commit ()
+conn.close ()
