60dee35442f24613e9a7b33a8ad94dcce50f451f
Samuel <samuelti0410@gmail.com>
2026-04-21 15:36:14 +0800

    COMMIT_EDITMSG

--- src/database.py
+++ src/database.py
@@ -1,9 +1,9 @@
 import sqlite3
-conn = sqlite3.connect ("tracker.db")
+conn = sqlite3.connect ("tracker1.db")
 cursor = conn.cursor ()
 
 cursor.execute ("""
-                CREATE TABLE IF NOT EXISTS tracker(
+                CREATE TABLE IF NOT EXISTS tracker1(
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 topic TEXT,
                 duration INTEGER,
@@ -11,16 +11,26 @@ cursor.execute ("""
                 )
 """)
 
+X = input("What subject are you studying right now?")
+Y = input("How long did you study?")
+Z = input("What is your weakness level in this test?")
+
 cursor.execute ("""
-INSERT INTO tracker (topic, duration, weakness_level)
+INSERT INTO tracker1 (topic, duration, weakness_level)
 VALUES (?, ?, ?)
 """,(X, Y, Z)
 )
 
 cursor.execute ("""
-SELECT * FROM tracker
-                """)
-print (cursor.fetchall())
+SELECT * FROM tracker1 WHERE topic = ?
+                """, (X,))
+print ("Record for this subject is: ",cursor.fetchall())
+
+cursor.execute ("""
+SELECT COUNT (*) FROM tracker1 WHERE topic = ?
+                """, (X,))
+COUNT = cursor.fetchone()[0]
+print (f"You have studied {X} {COUNT} times.")
 
 conn.commit ()
 conn.close ()
