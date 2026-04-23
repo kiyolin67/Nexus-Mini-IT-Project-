bdf3e38bec6ff1c9f4bf2a016c71c531827474c2
Samuel <samuelti0410@gmail.com>
2026-04-23 18:36:09 +0800

    .

--- src/database.py
+++ src/database.py
@@ -1,5 +1,5 @@
 import sqlite3
-conn = sqlite3.connect ("tracker1.db")
+conn = sqlite3.connect ("tracker2.db")
 cursor = conn.cursor ()
 
 cursor.execute ("""
@@ -16,18 +16,18 @@ Y = input("How long did you study?")
 Z = input("What is your weakness level in this test?")
 
 cursor.execute ("""
-INSERT INTO tracker1 (topic, duration, weakness_level)
+INSERT INTO tracker2 (topic, duration, weakness_level)
 VALUES (?, ?, ?)
 """,(X, Y, Z)
 )
 
 cursor.execute ("""
-SELECT * FROM tracker1 WHERE topic = ?
+SELECT * FROM tracker2 WHERE topic = ?
                 """, (X,))
 print ("Record for this subject is: ",cursor.fetchall())
 
 cursor.execute ("""
-SELECT COUNT (*) FROM tracker1 WHERE topic = ?
+SELECT COUNT (*) FROM tracker2 WHERE topic = ?
                 """, (X,))
 COUNT = cursor.fetchone()[0]
 print (f"You have studied {X} {COUNT} times.")
